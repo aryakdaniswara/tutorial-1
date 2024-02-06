6229b81d9a8d602fd3d1f5fe14e4d07553f62cc8
aryakdaniswara <arya.kdaniswara@gmail.com>
2024-02-07 02:06:54 +0700

    add product id and edit in product repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -6,17 +6,32 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    private static int productCount = 0;
 
     public Product create(Product product) {
+        product.setProductId(Integer.toString(productCount)); // make product id
+        productCount++;
         productData.add(product);
         return product;
     }
 
     public  Iterator<Product> findAll() {
         return  productData.iterator();
+    }
+
+    public Product edit(Product product) {
+        String productId = product.getProductId();
+        for (int i = 0; i < productData.size(); i++) {
+            if (productData.get(i).getProductId().equals(product.getProductId())) {
+                productData.set(i, product);
+                return product;
+            }
+        }
+        throw new NoSuchElementException("Product not found with ID: " + productId);
     }
 }
