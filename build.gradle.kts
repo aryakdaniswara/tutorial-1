d448d85a1beb8b2a5d82c8608660b1dbdfaa04b2
aryakdaniswara <arya.kdaniswara@gmail.com>
2024-02-14 17:00:09 +0700

    add sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -43,6 +44,14 @@ dependencies {
 	testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "aryakdaniswara_tutorial-1")
+		property("sonar.organization", "aryakdaniswara")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
 
 tasks.register<Test>("unitTest") {
